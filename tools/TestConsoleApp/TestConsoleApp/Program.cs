28681cbe61b7aa0d5f9b1d0e508f855e75cb6545
MartinHascak <martinhascak@gmail.com>
2022-07-28 07:21:05 +0200

    Test

--- tools/TestConsoleApp/TestConsoleApp/Program.cs
+++ tools/TestConsoleApp/TestConsoleApp/Program.cs
@@ -1,3 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Reflection;
+using System.Text.RegularExpressions;
+
 Console.WriteLine("Hello, World!");
+Console.WriteLine(Assembly.GetExecutingAssembly().Location);
+Console.WriteLine(GetCodeBaseRootPath());
+Console.WriteLine(NormalizeSearchPath("tools/test.cs"));
+
+static string GetCodeBaseRootPath()
+{
+    var executionAssemblyLocation = Assembly.GetExecutingAssembly().Location;
+    var appPathMatcher = new Regex(@"(?<!fil)[A-Za-z]:\\+[\S\s]*?(?=\\+tools)");
+    return appPathMatcher.Match(executionAssemblyLocation).Value;
+}
+
+static string NormalizeSearchPath(string path)
+{
+    var relativePath = NormalizeDirectoryPath(path.Replace(GetCodeBaseRootPath(), "", StringComparison.OrdinalIgnoreCase));
+    return $"./{relativePath}";
+}
+
+static string NormalizeDirectoryPath(string path) => path.Replace(Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar);
